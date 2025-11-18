0eb4a47a843d1c166a1b7b8254cb9525523d0527
unknown <dubuissonjeantivemary@gmail.com>
2025-11-18 13:51:50 -0500

    troisieme commit

--- app/src/main/java/com/example/monapp/MainActivity.kt
+++ app/src/main/java/com/example/monapp/MainActivity.kt
@@ -14,16 +14,32 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
-        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val flashcardQuestion = findViewById<TextView>(R.id.question)
+        val flashcardtext1 = findViewById<TextView>(R.id.text1)
+        val flashcardtext2 = findViewById<TextView>(R.id.text2)
+        val flashcardtext3 = findViewById<TextView>(R.id.text3)
+
 
         flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
+            flashcardtext1.visibility = View.VISIBLE
+            flashcardtext2.visibility = View.VISIBLE
+            flashcardtext3.visibility = View.VISIBLE
         }
-        flashcardAnswer.setOnClickListener {
-            flashcardQuestion.visibility = View.VISIBLE
-            flashcardAnswer.visibility = View.INVISIBLE
+
+        flashcardtext1.setOnClickListener {
+            findViewById<View>(R.id.text1).setBackgroundColor(getResources().getColor(R.color.Red, null))
+            findViewById<View>(R.id.text2).setBackgroundColor(getResources().getColor(R.color.Pink, null))
+            findViewById<View>(R.id.text3).setBackgroundColor(getResources().getColor(R.color.Green, null))
+        }
+        flashcardtext2.setOnClickListener {
+            findViewById<View>(R.id.text2).setBackgroundColor(getResources().getColor(R.color.Red, null))
+            findViewById<View>(R.id.text1).setBackgroundColor(getResources().getColor(R.color.Pink, null))
+            findViewById<View>(R.id.text3).setBackgroundColor(getResources().getColor(R.color.Green, null))
+        }
+        flashcardtext3.setOnClickListener {
+            findViewById<View>(R.id.text3).setBackgroundColor(getResources().getColor(R.color.Green, null))
+            findViewById<View>(R.id.text1).setBackgroundColor(getResources().getColor(R.color.Pink, null))
+            findViewById<View>(R.id.text2).setBackgroundColor(getResources().getColor(R.color.Pink, null))
         }
     }
 }
